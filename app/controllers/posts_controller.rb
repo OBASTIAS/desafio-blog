cdd71b544e39d839daf8b1e379809c1326195869
oscar bastias <oscarbastias@ip-192-168-1-3.sa-east-1.compute.internal>
2021-09-04 12:53:34 -0400

    first commit to push to heroku

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+
+  def create
+    @post = Post.create(
+      title: params['title'],
+      content: params['content'],
+      image: params['image']
+    )
+    @post.content.gsub!("spoiler"," ")
+    if @post.save
+      redirect_to root_path
+    end
+
+  end
+end
